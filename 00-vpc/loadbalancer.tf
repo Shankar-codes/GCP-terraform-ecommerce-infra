3435d750cd5fa664ed591e4901199cca2c009562
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 17:00:56 +0530

    GCP terraform ecommerce Infrastructure

--- 00-vpc/loadbalancer.tf
+++ 00-vpc/loadbalancer.tf
@@ -1,4 +1,5 @@
-#load balancer creation
+# load balancer creation
+# Backend service
 resource "google_compute_backend_service" "web" {
   name                  = "web-backend"
   protocol              = "HTTP"
@@ -16,12 +17,29 @@ resource "google_compute_backend_service" "web" {
   }
 }
 
+# URL Map
 resource "google_compute_url_map" "web" {
   name            = "web-url-map"
   default_service = google_compute_backend_service.web.id
 }
 
+# HTTP Proxy
 resource "google_compute_target_http_proxy" "web" {
   name    = "web-http-proxy"
   url_map = google_compute_url_map.web.id
+}
+
+# Global IP address
+resource "google_compute_global_address" "web" {
+  name = "web-global-ip"
+}
+
+# Global forwarding rule
+resource "google_compute_global_forwarding_rule" "web" {
+  name                  = "web-forwarding-rule"
+  target                = google_compute_target_http_proxy.web.id
+  port_range            = "80"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  ip_address            = google_compute_global_address.web.id
 }
