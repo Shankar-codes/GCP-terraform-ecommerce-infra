e6dcd672cad851c1bf9f819a8ce4ee5daac54865
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 15:11:00 +0530

    GCP terraform ecommerce Infrastructure

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -27,3 +27,18 @@ resource "google_compute_router" "ecommerce_router" {
   network = google_compute_network.ecommerce_vpc.id
   region  = var.region
 }
+
+# NAT creation for private subnet
+resource "google_compute_router_nat" "ecommerce_nat" {
+  name                               = "ecommerce-nat"
+  router                             = google_compute_router.ecommerce_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
