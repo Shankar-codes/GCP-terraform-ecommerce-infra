5c85b5a58e297ef0a52e4c6e735a4cfe9431f2ae
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 16:57:45 +0530

    GCP terraform ecommerce Infrastructure

---
+++ 00-vpc/loadbalancer.tf
@@ -0,0 +1,17 @@
+#load balancer creation
+resource "google_compute_backend_service" "web" {
+  name                  = "web-backend"
+  protocol              = "HTTP"
+  port_name             = "http"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  health_checks = [
+    google_compute_health_check.web.id
+  ]
+
+  backend {
+    group           = google_compute_region_instance_group_manager.web.instance_group
+    balancing_mode  = "UTILIZATION"
+    max_utilization = 0.80
+  }
+}
