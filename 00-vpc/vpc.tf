725d33303557f78064a1131d8e9363041b941447
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 15:09:35 +0530

    GCP terraform ecommerce Infrastructure

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,9 +1,11 @@
+# vpc creation
 resource "google_compute_vpc" "ecommerce_vpc" {
   name = "ecommerce-vpc"
   auto_create_subnetworks = false
   routing_mode            = "GLOBAL"
 }
 
+# public subnet creation
 resource "google_compute_subnetwork" "public" {
   name          = "public-subnet"
   ip_cidr_range = var.public_subnet_cidr
@@ -11,6 +13,7 @@ resource "google_compute_subnetwork" "public" {
   network       = google_compute_network.ecommerce_vpc.id
 }
 
+# private subnet creation
 resource "google_compute_subnetwork" "private" {
   name          = "private-subnet"
   ip_cidr_range = var.private_subnet_cidr
@@ -18,3 +21,9 @@ resource "google_compute_subnetwork" "private" {
   network       = google_compute_network.ecommerce_vpc.id
 }
 
+# router creation
+resource "google_compute_router" "ecommerce_router" {
+  name    = "ecommerce-router"
+  network = google_compute_network.ecommerce_vpc.id
+  region  = var.region
+}
