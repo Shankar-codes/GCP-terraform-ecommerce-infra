e5e6aab6edb3962dee6125ef045993a9cf85adf7
Shankar-codes <harikainteriors@gmail.com>
2026-08-10 15:00:50 +0530

    GCP terraform ecommerce Infrastructure

---
+++ 00-vpc/variables.tf
@@ -0,0 +1,22 @@
+variable project_id {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable region {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR range for public subnet"
+  type        = string
+  default     = "10.10.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR range for private subnet"
+  type        = string
+  default     = "10.10.2.0/24"
+}
